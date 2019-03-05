58a350393adb24ed6e79d9ff0e41060a03f3e518
xyh-cosmo <yhxu@nao.cas.cn>
2019-03-05 23:22:47 +0800

    ...

--- 20190226/example_results/plot_BAO_Hz.py
+++ 20190226/example_results/plot_BAO_Hz.py
@@ -1,2 +1,116 @@
 from pylab import *
+from scipy.interpolate import interp1d
 
+def read_JLA_mock( mock_filename ):
+	fp = open(mock_filename,'r')
+	lines = fp.readlines()
+	fp.close()
+
+	snls3 = []
+	for line in lines:
+		sn = line.split()
+		temp = []
+		temp.append(float(sn[1]))
+		temp.append(float(sn[2]))
+		temp.append(float(sn[3]))
+		temp.append(float(sn[4]))
+		snls3.append(temp)
+
+	return np.array(snls3)
+
+Hz_best_fit_lcdm = loadtxt('/Users/xyh/GitHub/ClassMC-dev/best_fit/LCDM_Hz.txt')
+Hz_best_fit_wzcdm = loadtxt('/Users/xyh/GitHub/ClassMC-dev/best_fit/wzCDM_Hz.txt')
+Hz_lcdm = interp1d(Hz_best_fit_lcdm[:,0],Hz_best_fit_lcdm[:,1])
+Hz_wzcdm = interp1d(Hz_best_fit_wzcdm[:,0],Hz_best_fit_wzcdm[:,1])
+
+
+bao_best_fit_lcdm = loadtxt('/Users/xyh/GitHub/ClassMC-dev/best_fit/LCDM_BAO.txt')
+bao_best_fit_wzcdm = loadtxt('/Users/xyh/GitHub/ClassMC-dev/best_fit/wzCDM_BAO.txt')
+DA_lcdm = interp1d(bao_best_fit_lcdm[:,0],bao_best_fit_lcdm[:,6])
+DA_wzcdm = interp1d(bao_best_fit_wzcdm[:,0],bao_best_fit_wzcdm[:,6])
+
+
+z_ref = linspace(1e-3,2.55,30)
+
+#######################
+# configure figure size
+fig = figure(figsize=(6,5))
+
+colors = ['gray','g','y']
+
+#######################
+# load BAO measurments
+rd_fid_lcdm = 150.617
+rd_fid_wzcdm = 150.586
+
+# bao_dr12 = loadtxt('/Users/xyh/GitHub/ClassMC-dev/mock_samples/GenBAOs/template/DR12_tomoBAO_Zhao_measurement.txt')
+# DA_lya = loadtxt('/Users/xyh/GitHub/ClassMC-dev/data/BAO/Lya.txt')
+bao_dr12 = loadtxt('/Users/xyh/GitHub/ClassMC-dev/mock_samples/GenBAOs/mock_Dr12_BAO_20190226/DR12_36.dat')
+DA_lya = loadtxt('/Users/xyh/GitHub/ClassMC-dev/mock_samples/GenBAOs/mock_z2.34/LyAlpha_36.dat')
+
+MB = -19.3
+sn_data = read_JLA_mock('/Users/xyh/GitHub/ClassMC-dev/mock_samples/mock_JLA_20190227_full_cov/MOCK_JLA_36.txt')
+sn_z = sn_data[:,0]
+sn_mu = sn_data[:,1] + MB*0
+sn_DL = 10**(sn_mu/5)*1e-5
+sn_DA = sn_DL/(1+sn_z)**2
+
+bao_z = bao_dr12[:9,0]
+bao_dr12_DA = bao_dr12[:9,1]*rd_fid_lcdm 
+bao_dr12_Hz = bao_dr12[9:,1]/rd_fid_lcdm 
+
+bao_dr12_DA_err = loadtxt('BAO_DA_rs_std.txt')
+bao_dr12_DA_err *= rd_fid_lcdm
+
+ax = fig.add_subplot(2,1,1)
+
+ax.plot(sn_z,sn_DA/DA_lcdm(sn_z),'.',color=colors[0],alpha=0.4,label='JLA SNe Ia')
+ax.errorbar(bao_z,bao_dr12_DA/DA_lcdm(bao_z),yerr=bao_dr12_DA_err/DA_lcdm(bao_z),fmt='o',color=colors[1],label=r'DR12 BAO')
+ax.errorbar(DA_lya[0],DA_lya[1]/DA_lcdm(DA_lya[0]),yerr=DA_lya[2]/DA_lcdm(DA_lya[0]),fmt='d',color=colors[2],label=r'Ly-$\alpha$ forest')
+ax.plot(z_ref,DA_lcdm(z_ref)/DA_lcdm(z_ref),'b--')
+ax.plot(z_ref,DA_wzcdm(z_ref)/DA_lcdm(z_ref),'r-')
+
+ax.set_xlim(-0.025,2.525)
+ax.set_xticks([])
+ax.set_xlabel(r'$Z$')
+ax.set_ylim(0.8975,1.1025)
+yticks = [0.9,0.95,1.0,1.05,1.1]
+ax.set_yticks(yticks)
+ax.set_yticklabels(yticks,fontsize=13)
+ax.set_ylabel(r'$D_{A}(z)/D_{A,LCDM}(z)$',fontsize=13)
+ax.legend(loc='upper right',ncol=1,frameon=True,fontsize=13)
+# ax.grid(linestyle='dashed')
+
+#######################
+# load H(z)
+bao_dr12_Hz_err = loadtxt('BAO_DA_rs_std.txt')
+bao_dr12_Hz_err /= rd_fid_lcdm
+
+# Hz_data = loadtxt('/Users/xyh/GitHub/ClassMC-dev/mock_samples/GenHz/template/Hz_all.dat')
+Hz_data = loadtxt('/Users/xyh/GitHub/ClassMC-dev/mock_samples/GenHz/mock_Hz_20190226/Hz_36.dat')
+Hz_z = Hz_data[:,0]
+
+ax = fig.add_subplot(2,1,2)
+ax.errorbar(bao_z,bao_dr12_Hz/Hz_lcdm(bao_z),yerr=bao_dr12_Hz_err/Hz_lcdm(bao_z),fmt='d',color=colors[1],label=r'DR12 BAO')
+ax.errorbar(Hz_z,Hz_data[:,1]/Hz_lcdm(Hz_z),yerr=Hz_data[:,2]/Hz_lcdm(Hz_z),fmt='o',color=colors[0],label='OHD')
+ax.plot(z_ref,Hz_lcdm(z_ref)/Hz_lcdm(z_ref),'b--')
+ax.plot(z_ref,Hz_wzcdm(z_ref)/Hz_lcdm(z_ref),'r-')
+
+ax.set_xlim(-0.025,2.525)
+xticks = [0,0.5,1,1.5,2,2.5]
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticks,fontsize=13)
+ax.set_xlabel(r'$Z$')
+ax.set_ylim(0.74,1.26)
+yticks = [0.8,0.9,1.0,1.1,1.2]
+ax.set_yticks(yticks)
+ax.set_yticklabels(yticks,fontsize=13)
+ax.set_ylabel(r'$H(z)/H_{LCDM}(z)$',fontsize=13)
+ax.legend(loc='upper right',ncol=1,frameon=True,fontsize=13)
+# ax.grid(linestyle='dashed')
+
+fig.subplots_adjust(top=0.995,left=0.125,right=0.975,bottom=0.1,hspace=0)
+
+fig.savefig('example_fit_BAO_Hz.pdf')
+
+show()
--- 20190226/example_results/plot_example_eos.py
+++ 20190226/example_results/plot_example_eos.py
@@ -16,7 +16,7 @@ zmax = 2.5
 a = linspace(1,1/(1+zmax),30)
 z = 1/a-1
 
-fig = figure(figsize=(7,5*(7./8)))
+fig = figure(figsize=(6,5))
 
 ax = fig.add_subplot(1,1,1)
 
@@ -30,9 +30,9 @@ ax.hlines(-1,xmin=0,xmax=zmax,linestyles='--',colors='b',label=r'Fiducial $w=-1$
 
 ax.set_xlim(-0.025,zmax+0.025)
 
-ax.set_xlabel(r'$z$',fontsize=14)
-ax.set_ylabel(r'$w(z)$',fontsize=14)
-lgd=ax.legend(loc='upper left',frameon=False,fontsize=14)
+ax.set_xlabel(r'$z$',fontsize=13)
+ax.set_ylabel(r'$w(z)$',fontsize=13)
+lgd=ax.legend(loc='upper left',frameon=False,fontsize=13)
 texts = lgd.get_texts()
 setp(texts[0],fontsize=14,color='r')
 setp(texts[1],fontsize=14,color='b')
@@ -40,33 +40,28 @@ setp(texts[1],fontsize=14,color='b')
 # text(1.5,-1.25,r'$\chi^2_{\rm LCDM} = ' + str(round(chisq_lcdm,3)) + '$',fontsize=14)
 # text(1.5,-1.45,r'$\chi^2_{\rm w(z)CDM} = ' + str(round(chisq_wzCDM,3)) + '$',fontsize=14)
 
-chisq_val = '  %8.3f'%(chisq_prior)
-ax.text(1.7,-1.2,r'$\chi^2_{\rm prior}$',fontsize=13)
-ax.text(1.95,-1.2,r'$=$',fontsize=13)
-ax.text(2.1,-1.2,chisq_val,fontsize=13)
-
-chisq_val = '%8.3f'%(chisq_wzCDM+chisq_prior)
-ax.text(1.2,-1.4,r'$\chi^2_{\rm w(z)CDM} + \chi^2_{\rm prior}$',fontsize=13)
-ax.text(1.95,-1.4,r'$=$',fontsize=13)
-ax.text(2.1,-1.4,chisq_val,fontsize=13)
-
 chisq_val = ' %8.3f'%(chisq_wzCDM-chisq_lcdm)
-ax.text(1.2,-1.6,r'$\chi^2_{\rm w(z)CDM} - \chi^2_{\rm LCDM}$',fontsize=13)
-ax.text(1.95,-1.6,r'$=$',fontsize=13)
-ax.text(2.1,-1.6,chisq_val,fontsize=13)
+ax.text(1.5,-1.6,r'$\Delta \chi^2_{\rm data}$',fontsize=15)
+ax.text(1.85,-1.6,r'$=$',fontsize=15)
+ax.text(1.9,-1.6,chisq_val,fontsize=15)
 
-ax.text(0.075,-0.2,r'$p$-value = '+p_all+' (All)',fontsize=13)
-ax.text(0.075,-0.35,r'$p$-value = '+p_sn+' (SNe)',fontsize=13)
+chisq_val = ' %8.3f'%(chisq_prior+chisq_wzCDM-chisq_lcdm)
+ax.text(1.2,-1.8,r'$\Delta \chi^2_{\rm data} + \chi^2_{\rm prior}$',fontsize=15)
+ax.text(1.85,-1.8,r'$=$',fontsize=15)
+ax.text(1.9,-1.8,chisq_val,fontsize=15)
+
+ax.text(0.075,-0.2,r'$p$-value = '+p_all+' (All)',fontsize=14)
+ax.text(0.075,-0.35,r'$p$-value = '+p_sn+' (SNe)',fontsize=14)
 
 xticks = [0,0.5,1,1.5,2,2.5]
 ax.set_xticks(xticks)
-ax.set_xticklabels(xticks,fontsize=14)
+ax.set_xticklabels(xticks,fontsize=13)
 
-yticks = [-1.5,-1,-0.5,0]
+yticks = [-2,-1.5,-1,-0.5,0]
 ax.set_yticks(yticks)
-ax.set_yticklabels(yticks,fontsize=14)
+ax.set_yticklabels(yticks,fontsize=13)
 
-fig.subplots_adjust(top=0.995)
+fig.subplots_adjust(top=0.995,bottom=0.1,left=0.125,right=0.975)
 
 fig.savefig('example_w_result.pdf')
 
